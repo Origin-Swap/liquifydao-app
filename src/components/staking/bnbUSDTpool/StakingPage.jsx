0f1eb00c8a66e5e9e5d484fd1bcd2e4ce8394ddd
origin-swap <ah0229247@gmail.com>
2026-02-16 06:28:44 +0700

    update

--- src/components/staking/bnbUSDTpool/StakingPage.jsx
+++ src/components/staking/bnbUSDTpool/StakingPage.jsx
@@ -7,7 +7,7 @@ import {
 
 export const StakingBNBUSDTPage = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#0f172a] via-[#111827] to-[#0b1120] text-white px-4 py-10">
+    <div>
 
         <div className="grid md:grid-cols-2 gap-8">
           <BnbPoolCard contractAddress={BNB_STAKING_ADDRESS} />
